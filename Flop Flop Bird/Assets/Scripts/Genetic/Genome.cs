d70ed797564a77f0d0de9d5ad4c4c7f2fb82ad80
Guillaume <g.singland@gmail.com>
2017-03-29 11:00:59 -0400

    Updating the genome class

--- Flop Flop Bird/Assets/Scripts/Genetic/Genome.cs
+++ Flop Flop Bird/Assets/Scripts/Genetic/Genome.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Genome : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+/*
+ * A Genome is the description of a bird's neural network.
+ */
+public class Genome {
+
+	// The number of layers in the neural network, including the input and the output
+	public int nbLayers;
+
+	// The number of neurons in each layer
+	public List<int> neuronsPerLayer;
+
+	// The weights of the neurons
+	public List<float> weights;
+
+	// Clone the genome
+	public Genome Clone() {
+		// TODO (Guillaume)
 	}
 }
